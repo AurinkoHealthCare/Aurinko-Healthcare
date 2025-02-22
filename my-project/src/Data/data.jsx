93c1a1e9c214162a57e11eae0ae15b0a89453a94
Aurinko Healthcare Pvt . LTD. <aurinkohealthcare2014@gmail.com>
2025-02-22 19:17:24 +0530

    home page complete

--- my-project/src/Data/data.jsx
+++ my-project/src/Data/data.jsx
@@ -5,7 +5,7 @@ export const block3Data = [
     hoverImage: "/Assets/Product img/Auribery Plus-hover.jpg",
     title: "Auriplanta",
     segment: "LiveStock",
-    rating: 4.5,
+    rating: 4.5
   },
   {
     id: 2,
@@ -13,7 +13,7 @@ export const block3Data = [
     hoverImage: "/Assets/Product img/Auribery Plus-hover.jpg",
     title: "Nomino",
     segment: "LiveStock & Poultry",
-    rating: 4.0,
+    rating: 4.0
   },
   {
     id: 3,
@@ -21,7 +21,7 @@ export const block3Data = [
     hoverImage: "/Assets/Product img/Auribery Plus-hover.jpg",
     title: "Metagrow WS",
     segment: "Poultry",
-    rating: 4.5,
+    rating: 4.5
   },
   {
     id: 4,
@@ -37,7 +37,7 @@ export const block3Data = [
     hoverImage: "/Assets/Product img/Auribery Plus-hover.jpg",
     title: "Auribery Plus",
     segment: "Human",
-    rating: 4.5,
+    rating: 4.5
   }
 ];
 
@@ -49,7 +49,7 @@ export const block8Data = [
     hoverImage: "/Assets/Product img/Auribery Plus-hover.png",
     title: "Auritrol",
     segment: "Health Supplements",
-    rating: 4.5,
+    rating: 4.5
   },
   {
     id: 2,
@@ -57,7 +57,7 @@ export const block8Data = [
     hoverImage: "/Assets/Product img/Auribery Plus-hover.png",
     title: "Aurijoint",
     segment: "Health Supplements",
-    rating: 4.0,
+    rating: 4.0
   },
   {
     id: 3,
@@ -65,7 +65,7 @@ export const block8Data = [
     hoverImage: "/Assets/Product img/Auribery Plus-hover.png",
     title: "Auricur Plus",
     segment: "Health Supplements",
-    rating: 4.5,
+    rating: 4.5
   },
   {
     id: 4,
@@ -73,7 +73,7 @@ export const block8Data = [
     hoverImage: "/Assets/Product img/Auribery Plus-hover.png",
     title: "Auriglow massage Oil",
     segment: "Personal Care",
-    rating: 4.0,
+    rating: 4.0
   },
   {
     id: 5,
@@ -81,9 +81,10 @@ export const block8Data = [
     hoverImage: "/Assets/Product img/Auribery Plus-hover.png",
     title: "Auriglow anti marks oil",
     segment: "Personal Care",
-    rating: 4.5,
+    rating: 4.5
   }
 ];
+
 
 export const productTypes = [
   {
@@ -91,118 +92,126 @@ export const productTypes = [
     description:
       "Our powder and granule forms are ideal for quick, uniform mixing into animal feed or water. These products are formulated for high bioavailability, ensuring that nutrients are efficiently absorbed into the animal's system. Supporting digestive health, immune function, and growth. Whether you're supplementing livestock, poultry, aqua, pet, swine powders and granules provide an effective and reliable option for meeting your animals’ nutritional needs.",
     image: "/Assets/block 5/Powder.jpg",
-    alt: "Powder Granules",
+    alt: "Powder Granules"
   },
   {
     title: "BOLUSES / TABLETS / CAPSULES",
     description:
       "Boluses, capsules, and tablets are highly effective forms of animal nutrition designed for precise, controlled dosing. These solid supplements provide slow-release nutrients, ensuring a steady and prolonged effect. Boluses are ideal for larger animals like cattle and horses, as they can deliver a high concentration of active ingredients with minimal administration effort. Capsules and tablets are often used for smaller doses and are easy to administer, making them ideal for pets, poultry, and smaller livestock. These forms allow for targeted supplementation of essential vitamins, minerals, or medication, ensuring that animals receive the exact nutrients they need at the right time for optimal health and performance.",
     image: "/Assets/block 5/tablets.jpg",
-    alt: "Tablets and Capsules",
+    alt: "Tablets and Capsules"
   },
   {
     title: "LIQUIDS",
     description:
       "Liquids are particularly effective for hydration, electrolyte balance, immune support, and energy boosts, making them a versatile option for livestock, poultry, pets, and aquaculture, swine and human OTC. Whether used as a protective barrier or a calming treatment, liquid-based repellents and oils ensure that animals remain comfortable and healthy, while also enhancing their overall well-being.",
     image: "/Assets/block 5/Liquid.jpg",
-    alt: "Liquid Products",
+    alt: "Liquid Products"
   },
   {
     title: "GELS",
     description:
       "Our Gel-based animal nutrition products provide are formulated for instant bioavailability and targeted support. Gels are versatile, commonly used for hydration, immune health and recovery, making them effective for livestock. Fast-acting nature and convenience make Gel a valuable solution for delivering essential nutrients when needed most.",
     image: "/Assets/block 5/gel.jpg",
-    alt: "Gel Formulation",
+    alt: "Gel Formulation"
   },
   {
     title: "SPRAYS",
     description:
       "Spray-based animal nutrition products offer a convenient and efficient way to deliver nutrients or provide protective treatments to animals. These products can be easily applied to the skin, coat, or directly onto feed, ensuring even distribution of active ingredients. Spray formulations are particularly effective for hydrating animals, repelling pests, or providing topical care for wounds and skin conditions.",
     image: "/Assets/block 5/spray.jpg",
-    alt: "Spray Bottle",
-  },
+    alt: "Spray Bottle"
+  }
 ];
+
 
 export const speciesData = [
   {
     title: "Poultry",
     image: "/Assets/block 5/Block5 img1.jpg",
     data: [
-      { type: "Corn", amount: "50kg" },
-      { type: "Soybean", amount: "30kg" },
-    ],
+      { type: "Broilers, Chicks, Finisher, Grower", amount: "Weak Immunity", products: "MetaMune-VET (Immunomodulator), Aurivir-S, Aurivir-R, MetaBlu Plus, Aurimune" },
+      { type: "Broilers, Chicks, Finisher, Grower", amount: "Liver Protectant", products: "Livauro Forte  AURILIV Vet, Natuchol" },
+      { type: "Broilers, Chicks, Finisher, Grower", amount: "Weak Immunity", products: "MetaMune-VET (Immunomodulator), Aurivir-S, Aurivir-R, MetaBlu Plus, Aurimune" },
+    ]
   },
   {
     title: "Aqua",
     image: "/Assets/block 5/Block5 img2.jpg",
     data: [
-      { type: "Wheat", amount: "20kg" },
-      { type: "Barley", amount: "40kg" },
-    ],
+      { type: "Broilers, Chicks, Finisher, Grower", amount: "Weak Immunity", products: "MetaMune-VET (Immunomodulator), Aurivir-S, Aurivir-R, MetaBlu Plus, Aurimune" },
+      { type: "Broilers, Chicks, Finisher, Grower", amount: "Weak Immunity", products: "MetaMune-VET (Immunomodulator), Aurivir-S, Aurivir-R, MetaBlu Plus, Aurimune" },
+      { type: "Broilers, Chicks, Finisher, Grower", amount: "Weak Immunity", products: "MetaMune-VET (Immunomodulator), Aurivir-S, Aurivir-R, MetaBlu Plus, Aurimune" }
+    ]
   },
   {
     title: "Swine",
     image: "/Assets/block 5/Block5 img3.jpg",
     data: [
-      { type: "Maize", amount: "35kg" },
-      { type: "Oats", amount: "25kg" },
-    ],
+      { type: "Broilers, Chicks, Finisher, Grower", amount: "Weak Immunity", products: "MetaMune-VET (Immunomodulator), Aurivir-S, Aurivir-R, MetaBlu Plus, Aurimune" },
+      { type: "Broilers, Chicks, Finisher, Grower", amount: "Weak Immunity", products: "MetaMune-VET (Immunomodulator), Aurivir-S, Aurivir-R, MetaBlu Plus, Aurimune" },
+      { type: "Broilers, Chicks, Finisher, Grower", amount: "Weak Immunity", products: "MetaMune-VET (Immunomodulator), Aurivir-S, Aurivir-R, MetaBlu Plus, Aurimune" }
+    ]
   },
   {
     title: "Pet",
     image: "/Assets/block 5/Block5 img4.jpg",
     data: [
-      { type: "Alfalfa", amount: "28kg" },
-      { type: "Grass Hay", amount: "33kg" },
-    ],
+      { type: "Broilers, Chicks, Finisher, Grower", amount: "Weak Immunity", products: "MetaMune-VET (Immunomodulator), Aurivir-S, Aurivir-R, MetaBlu Plus, Aurimune" },
+      { type: "Broilers, Chicks, Finisher, Grower", amount: "Weak Immunity", products: "MetaMune-VET (Immunomodulator), Aurivir-S, Aurivir-R, MetaBlu Plus, Aurimune" },
+      { type: "Broilers, Chicks, Finisher, Grower", amount: "Weak Immunity", products: "MetaMune-VET (Immunomodulator), Aurivir-S, Aurivir-R, MetaBlu Plus, Aurimune" }
+    ]
   },
   {
     title: "Equine",
     image: "/Assets/block 5/Block5 img5.jpeg",
     data: [
-      { type: "Pellets", amount: "40kg" },
-      { type: "Hay", amount: "50kg" },
-    ],
+      { type: "Broilers, Chicks, Finisher, Grower", amount: "Weak Immunity", products: "MetaMune-VET (Immunomodulator), Aurivir-S, Aurivir-R, MetaBlu Plus, Aurimune" },
+      { type: "Broilers, Chicks, Finisher, Grower", amount: "Weak Immunity", products: "MetaMune-VET (Immunomodulator), Aurivir-S, Aurivir-R, MetaBlu Plus, Aurimune" },
+      { type: "Broilers, Chicks, Finisher, Grower", amount: "Weak Immunity", products: "MetaMune-VET (Immunomodulator), Aurivir-S, Aurivir-R, MetaBlu Plus, Aurimune" }
+    ]
   },
   {
     title: "Feed & Grain",
     image: "/Assets/block 5/Block5 img6.jpg",
     data: [
-      { type: "Grass", amount: "30kg" },
-      { type: "Pasture", amount: "40kg" },
-    ],
+      { type: "Broilers, Chicks, Finisher, Grower", amount: "Weak Immunity", products: "MetaMune-VET (Immunomodulator), Aurivir-S, Aurivir-R, MetaBlu Plus, Aurimune" },
+      { type: "Broilers, Chicks, Finisher, Grower", amount: "Weak Immunity", products: "MetaMune-VET (Immunomodulator), Aurivir-S, Aurivir-R, MetaBlu Plus, Aurimune" },
+      { type: "Broilers, Chicks, Finisher, Grower", amount: "Weak Immunity", products: "MetaMune-VET (Immunomodulator), Aurivir-S, Aurivir-R, MetaBlu Plus, Aurimune" }
+    ]
   },
 ];
+
 
 export const reviews = [
   {
       id: 1,
       name: "John Doe",
       image: "/data/user1.png",
-      text: "Excellent service! Highly recommended.",
+      text: "Excellent service! Highly recommended."
   },
   {
       id: 2,
       name: "Jane Smith",
       image: "/data/user2.png",
-      text: "Great experience, very professional team!",
+      text: "Great experience, very professional team!"
   },
   {
       id: 3,
       name: "Alex Johnson",
       image: "/data/user3.png",
-      text: "Quick and reliable healthcare services.",
+      text: "Quick and reliable healthcare services."
   },
   {
       id: 4,
       name: "Emma Brown",
       image: "/data/user4.png",
-      text: "Loved the friendly staff and fast service.",
+      text: "Loved the friendly staff and fast service."
   },
   {
       id: 5,
       name: "Michael Wilson",
       image: "/data/user5.png",
-      text: "Affordable and efficient medical solutions.",
-  },
+      text: "Affordable and efficient medical solutions."
+  }
 ];
