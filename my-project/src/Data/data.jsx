af9d833d9513ad797cea9308d4a99c924a74df9d
Aurinko Healthcare Pvt . LTD. <aurinkohealthcare2014@gmail.com>
2025-02-24 15:27:43 +0530

    home page complete

--- my-project/src/Data/data.jsx
+++ my-project/src/Data/data.jsx
@@ -5,7 +5,7 @@ export const block3Data = [
     hoverImage: "/Assets/Product img/Auribery Plus-hover.jpg",
     title: "Auriplanta",
     segment: "LiveStock",
-    rating: 4.5
+    rating: 4.5,
   },
   {
     id: 2,
@@ -13,7 +13,7 @@ export const block3Data = [
     hoverImage: "/Assets/Product img/Auribery Plus-hover.jpg",
     title: "Nomino",
     segment: "LiveStock & Poultry",
-    rating: 4.0
+    rating: 4.0,
   },
   {
     id: 3,
@@ -21,7 +21,7 @@ export const block3Data = [
     hoverImage: "/Assets/Product img/Auribery Plus-hover.jpg",
     title: "Metagrow WS",
     segment: "Poultry",
-    rating: 4.5
+    rating: 4.5,
   },
   {
     id: 4,
@@ -37,7 +37,7 @@ export const block3Data = [
     hoverImage: "/Assets/Product img/Auribery Plus-hover.jpg",
     title: "Auribery Plus",
     segment: "Human",
-    rating: 4.5
+    rating: 4.5,
   }
 ];
 
@@ -49,7 +49,7 @@ export const block8Data = [
     hoverImage: "/Assets/Product img/Auribery Plus-hover.png",
     title: "Auritrol",
     segment: "Health Supplements",
-    rating: 4.5
+    rating: 4.5,
   },
   {
     id: 2,
@@ -57,7 +57,7 @@ export const block8Data = [
     hoverImage: "/Assets/Product img/Auribery Plus-hover.png",
     title: "Aurijoint",
     segment: "Health Supplements",
-    rating: 4.0
+    rating: 4.0,
   },
   {
     id: 3,
@@ -65,7 +65,7 @@ export const block8Data = [
     hoverImage: "/Assets/Product img/Auribery Plus-hover.png",
     title: "Auricur Plus",
     segment: "Health Supplements",
-    rating: 4.5
+    rating: 4.5,
   },
   {
     id: 4,
@@ -73,7 +73,7 @@ export const block8Data = [
     hoverImage: "/Assets/Product img/Auribery Plus-hover.png",
     title: "Auriglow massage Oil",
     segment: "Personal Care",
-    rating: 4.0
+    rating: 4.0,
   },
   {
     id: 5,
@@ -81,10 +81,9 @@ export const block8Data = [
     hoverImage: "/Assets/Product img/Auribery Plus-hover.png",
     title: "Auriglow anti marks oil",
     segment: "Personal Care",
-    rating: 4.5
+    rating: 4.5,
   }
 ];
-
 
 export const productTypes = [
   {
@@ -92,126 +91,215 @@ export const productTypes = [
     description:
       "Our powder and granule forms are ideal for quick, uniform mixing into animal feed or water. These products are formulated for high bioavailability, ensuring that nutrients are efficiently absorbed into the animal's system. Supporting digestive health, immune function, and growth. Whether you're supplementing livestock, poultry, aqua, pet, swine powders and granules provide an effective and reliable option for meeting your animals’ nutritional needs.",
     image: "/Assets/block 5/Powder.jpg",
-    alt: "Powder Granules"
+    alt: "Powder Granules",
   },
   {
     title: "BOLUSES / TABLETS / CAPSULES",
     description:
       "Boluses, capsules, and tablets are highly effective forms of animal nutrition designed for precise, controlled dosing. These solid supplements provide slow-release nutrients, ensuring a steady and prolonged effect. Boluses are ideal for larger animals like cattle and horses, as they can deliver a high concentration of active ingredients with minimal administration effort. Capsules and tablets are often used for smaller doses and are easy to administer, making them ideal for pets, poultry, and smaller livestock. These forms allow for targeted supplementation of essential vitamins, minerals, or medication, ensuring that animals receive the exact nutrients they need at the right time for optimal health and performance.",
     image: "/Assets/block 5/tablets.jpg",
-    alt: "Tablets and Capsules"
+    alt: "Tablets and Capsules",
   },
   {
     title: "LIQUIDS",
     description:
       "Liquids are particularly effective for hydration, electrolyte balance, immune support, and energy boosts, making them a versatile option for livestock, poultry, pets, and aquaculture, swine and human OTC. Whether used as a protective barrier or a calming treatment, liquid-based repellents and oils ensure that animals remain comfortable and healthy, while also enhancing their overall well-being.",
     image: "/Assets/block 5/Liquid.jpg",
-    alt: "Liquid Products"
+    alt: "Liquid Products",
   },
   {
     title: "GELS",
     description:
       "Our Gel-based animal nutrition products provide are formulated for instant bioavailability and targeted support. Gels are versatile, commonly used for hydration, immune health and recovery, making them effective for livestock. Fast-acting nature and convenience make Gel a valuable solution for delivering essential nutrients when needed most.",
     image: "/Assets/block 5/gel.jpg",
-    alt: "Gel Formulation"
+    alt: "Gel Formulation",
   },
   {
     title: "SPRAYS",
     description:
       "Spray-based animal nutrition products offer a convenient and efficient way to deliver nutrients or provide protective treatments to animals. These products can be easily applied to the skin, coat, or directly onto feed, ensuring even distribution of active ingredients. Spray formulations are particularly effective for hydrating animals, repelling pests, or providing topical care for wounds and skin conditions.",
     image: "/Assets/block 5/spray.jpg",
-    alt: "Spray Bottle"
-  }
+    alt: "Spray Bottle",
+  },
 ];
-
 
 export const speciesData = [
   {
     title: "Poultry",
-    image: "/Assets/block 5/Block5 img1.jpg",
-    data: [
-      { type: "Broilers, Chicks, Finisher, Grower", amount: "Weak Immunity", products: "MetaMune-VET (Immunomodulator), Aurivir-S, Aurivir-R, MetaBlu Plus, Aurimune" },
-      { type: "Broilers, Chicks, Finisher, Grower", amount: "Liver Protectant", products: "Livauro Forte  AURILIV Vet, Natuchol" },
-      { type: "Broilers, Chicks, Finisher, Grower", amount: "Weak Immunity", products: "MetaMune-VET (Immunomodulator), Aurivir-S, Aurivir-R, MetaBlu Plus, Aurimune" },
-    ]
+    image: "/Assets/block 5/Block5 img1.jpg"
   },
   {
     title: "Aqua",
-    image: "/Assets/block 5/Block5 img2.jpg",
-    data: [
-      { type: "Broilers, Chicks, Finisher, Grower", amount: "Weak Immunity", products: "MetaMune-VET (Immunomodulator), Aurivir-S, Aurivir-R, MetaBlu Plus, Aurimune" },
-      { type: "Broilers, Chicks, Finisher, Grower", amount: "Weak Immunity", products: "MetaMune-VET (Immunomodulator), Aurivir-S, Aurivir-R, MetaBlu Plus, Aurimune" },
-      { type: "Broilers, Chicks, Finisher, Grower", amount: "Weak Immunity", products: "MetaMune-VET (Immunomodulator), Aurivir-S, Aurivir-R, MetaBlu Plus, Aurimune" }
-    ]
+    image: "/Assets/block 5/Block5 img2.jpg"
   },
   {
     title: "Swine",
-    image: "/Assets/block 5/Block5 img3.jpg",
-    data: [
-      { type: "Broilers, Chicks, Finisher, Grower", amount: "Weak Immunity", products: "MetaMune-VET (Immunomodulator), Aurivir-S, Aurivir-R, MetaBlu Plus, Aurimune" },
-      { type: "Broilers, Chicks, Finisher, Grower", amount: "Weak Immunity", products: "MetaMune-VET (Immunomodulator), Aurivir-S, Aurivir-R, MetaBlu Plus, Aurimune" },
-      { type: "Broilers, Chicks, Finisher, Grower", amount: "Weak Immunity", products: "MetaMune-VET (Immunomodulator), Aurivir-S, Aurivir-R, MetaBlu Plus, Aurimune" }
-    ]
+    image: "/Assets/block 5/Block5 img3.jpg"
   },
   {
     title: "Pet",
-    image: "/Assets/block 5/Block5 img4.jpg",
-    data: [
-      { type: "Broilers, Chicks, Finisher, Grower", amount: "Weak Immunity", products: "MetaMune-VET (Immunomodulator), Aurivir-S, Aurivir-R, MetaBlu Plus, Aurimune" },
-      { type: "Broilers, Chicks, Finisher, Grower", amount: "Weak Immunity", products: "MetaMune-VET (Immunomodulator), Aurivir-S, Aurivir-R, MetaBlu Plus, Aurimune" },
-      { type: "Broilers, Chicks, Finisher, Grower", amount: "Weak Immunity", products: "MetaMune-VET (Immunomodulator), Aurivir-S, Aurivir-R, MetaBlu Plus, Aurimune" }
-    ]
+    image: "/Assets/block 5/Block5 img4.jpg"
   },
   {
     title: "Equine",
-    image: "/Assets/block 5/Block5 img5.jpeg",
-    data: [
-      { type: "Broilers, Chicks, Finisher, Grower", amount: "Weak Immunity", products: "MetaMune-VET (Immunomodulator), Aurivir-S, Aurivir-R, MetaBlu Plus, Aurimune" },
-      { type: "Broilers, Chicks, Finisher, Grower", amount: "Weak Immunity", products: "MetaMune-VET (Immunomodulator), Aurivir-S, Aurivir-R, MetaBlu Plus, Aurimune" },
-      { type: "Broilers, Chicks, Finisher, Grower", amount: "Weak Immunity", products: "MetaMune-VET (Immunomodulator), Aurivir-S, Aurivir-R, MetaBlu Plus, Aurimune" }
-    ]
+    image: "/Assets/block 5/Block5 img5.jpeg"
   },
   {
     title: "Feed & Grain",
-    image: "/Assets/block 5/Block5 img6.jpg",
-    data: [
-      { type: "Broilers, Chicks, Finisher, Grower", amount: "Weak Immunity", products: "MetaMune-VET (Immunomodulator), Aurivir-S, Aurivir-R, MetaBlu Plus, Aurimune" },
-      { type: "Broilers, Chicks, Finisher, Grower", amount: "Weak Immunity", products: "MetaMune-VET (Immunomodulator), Aurivir-S, Aurivir-R, MetaBlu Plus, Aurimune" },
-      { type: "Broilers, Chicks, Finisher, Grower", amount: "Weak Immunity", products: "MetaMune-VET (Immunomodulator), Aurivir-S, Aurivir-R, MetaBlu Plus, Aurimune" }
-    ]
+    image: "/Assets/block 5/Block5 img6.jpg"
   },
 ];
 
-
 export const reviews = [
   {
-      id: 1,
-      name: "John Doe",
-      image: "/data/user1.png",
-      text: "Excellent service! Highly recommended."
+    id: 1,
+    name: "John Doe",
+    image: "/data/user1.png",
+    text: "Excellent service! Highly recommended.",
   },
   {
-      id: 2,
-      name: "Jane Smith",
-      image: "/data/user2.png",
-      text: "Great experience, very professional team!"
+    id: 2,
+    name: "Jane Smith",
+    image: "/data/user2.png",
+    text: "Great experience, very professional team!",
   },
   {
-      id: 3,
-      name: "Alex Johnson",
-      image: "/data/user3.png",
-      text: "Quick and reliable healthcare services."
+    id: 3,
+    name: "Alex Johnson",
+    image: "/data/user3.png",
+    text: "Quick and reliable healthcare services.",
   },
   {
-      id: 4,
-      name: "Emma Brown",
-      image: "/data/user4.png",
-      text: "Loved the friendly staff and fast service."
+    id: 4,
+    name: "Emma Brown",
+    image: "/data/user4.png",
+    text: "Loved the friendly staff and fast service.",
   },
   {
-      id: 5,
-      name: "Michael Wilson",
-      image: "/data/user5.png",
-      text: "Affordable and efficient medical solutions."
+    id: 5,
+    name: "Michael Wilson",
+    image: "/data/user5.png",
+    text: "Affordable and efficient medical solutions.",
+  },
+];
+
+export const products = [
+  {
+    id: 1,
+    name: "Supports Urinary Tract Health",
+    description:
+      "Auribery Plus is an advanced formulation of gut-acting probiotics enriched with Cranberry extract to support Urinary Tract Health and reduce UTI symptoms like pH, inflammation, etc.",
+    segment: "Human OTC",
+    type: "Capsule",
+    category: "Other Specialty",
+    packaging: "30, 60 and 90 Capsules Container",
+    Productimg: "/Assets/Product img/Auribery Plus.jpeg",
+    Productname: "/Assets/Product name/Auribery Plus.jpeg",
+    composition: [
+      "Each 500 mg Capsule contains:",
+      "Lactobacillus acidophilis 0.25 billion",
+      "Lactobacillus Plantarum 0.25 billion",
+      "Cranberry extract",
+      "(Proanthocyanidins will be 1.8%) 18 mg instead of 25%",
+      "Retinyl acetate (Vitamin A) 160 mcg",
+      "FOS 100 mg"
+    ],
+    indications:
+      "Reports suggest that Vaccinium macrocarpon extract supplementation supports Urinary Tract Health. Auribery is a natural plant active health supplement with technologically advanced bioenhancer and metabolic potentiator technology enriched with gut friendly probiotics and Vitamin A.",
+    usage:
+      "1 capsule twice daily during post-lunch/dinner. Or as directed by a qualified healthcare professional. This product is not intended to cure or prevent any disease.",
+    report: "Lab tested for purity and effectiveness.",
+    brochure: "Download our product brochure for details.",
+    feedback: "Customer reviews and testimonials."
+  },
+  {
+    id: 2,
+    name: "Green Tea Nanophosphosome®",
+    description:
+      "Auricam is a green tea supplement, a nano-emulsified EGCG (Epigallocatechin gallates) Phosphatidyl Choline-Serine-Ethanolamine complex enriched with Bioenhancer and Metabolic Potentiator technology. Auricam (Green tea Nanophosphosome) contains polyphenols i.e EGCG and caffeine, easily crosses the gut barrier resulting in significantly higher blood level. Potentiated phospholipid complex acts as carrier mediated transport system, which directs the EGCG and caffeine to reach the systemic circulation and also increases its absorption and bioavailability. Bioenhancer and Metabolic activator technology enhances the passive diffusion and active transport mechanism through intestinal microvilli to improve EGCG and caffeine absorption as well prevents its metabolic reduction through inhibition of P-gp efflux pump, CYP3AG and UGT.",
+    segment: "Human OTC",
+    type: "Capsule",
+    category: "Other Specialty",
+    packaging: "30, 60 and 90 Capsules Container",
+    Productimg: "/Assets/Product img/Auricam.jpeg",
+    Productname: "/Assets/Product name/Auricam.jpeg",
+    composition: [
+      "1 capsule",
+      "Green tea Nanophosphosome ® (Auricam®)",
+      "Green tea Phosphatidylcholine-, Ethanolamine-Inositol-Serine complex 300 mg",
+      "EGCG, minimum 50 mg",
+      "Daily Value not established * Fortified with Caffeine & Piperine",
+      "Other ingredients: Calcium Carbonate, Permitted class II preservative."
+    ],
+    indications:
+      "Reports suggest that consuming green tea extract helps in weight loss management. Auricam is a Nanophosphosome form of green tea with technologically advanced bioenhancer and metabolic potentiator technology.",
+    usage:
+      "2 capsules daily once or in divided doses. Or as directed by qualified healthcare professional. Better results observed in empty stomach or 1 hr interval from food intake. This product is not intended to cure or prevent any disease.",
+    report: "Lab tested for purity and effectiveness.",
+    brochure: "Download our product brochure for details.",
+    feedback: "Customer reviews and testimonials."
+  },
+  {
+    id: 3,
+    name: "Supports Urinary Tract HealthCurcumin Nanophosphosome®",
+    description:
+      "Auricur is Turmeric supplement, a nano-emulsified Curcumin Phosphatidyl Choline-Serine-Ethanolamine complex enriched with Bioenhancer and Metabolic Potentiator technology. Auricur (Curcumin Nanophosphosome) easily crosses the gut barrier resulting in significantly higher blood levels of Curcumin. Potentiated phospholipid complex acts as carrier mediated transport system, which directs the Curcumin to reach the systemic circulation and also increases it’s absorption and bioavailability. Bioenhancer and Metabolic potentiator technology enhances the permeability of intestinal microvilli and improves Curcumin absorption as well prevents it’s metabolic reduction through inhibition of P-gp, CYP3AG and UGT.",
+    segment: "Human OTC",
+    type: "Capsule , Tablet",
+    category: "Immunomodulator , Other Specialty",
+    packaging: "30, 60 and 90 tablet/Capsules Container",
+    Productimg: "/Assets/Product img/Auricur.jpeg",
+    Productname: "/Assets/Product name/Auricur.jpeg",
+    composition: [
+      "1 capsule",
+      "Green tea Nanophosphosome ® (Auricam®)",
+      "Green tea Phosphatidylcholine-, Ethanolamine-Inositol-Serine complex 300 mg",
+      "EGCG, minimum 50 mg",
+      "Daily Value not established * Fortified with Caffeine & Piperine",
+      "Other ingredients: Calcium Carbonate, Permitted class II preservative."
+    ],
+    indications:
+      "Reports suggest that Vaccinium macrocarpon extract supplementation supports Urinary Tract Health. Auribery is a natural plant active health supplement with technologically advanced bioenhancer and metabolic potentiator technology enriched with gut-friendly probiotics and Vitamin A.",
+    usage:
+      "1 capsule twice daily during post-lunch/dinner. Or as directed by a qualified healthcare professional. This product is not intended to cure or prevent any disease.",
+    report: "Lab tested for purity and effectiveness.",
+    brochure: "Download our product brochure for details.",
+    feedback: "Customer reviews and testimonials."
+  },
+  {
+    id: 4,
+    name: "Curcumin Nanophosphosome®  With Tinospora Cordifolia Extract",
+    description:
+      "Auricur Plus is a synergistic combination of Curcumin Nanophosphosome® (a nano-emulsified Curcumin Phosphatidyl Choline-Serine-Ethanolamine complex) and Tinospora cordifolia with technologically advanced bioenhancer and metabolic potentiator technology. Bioenhancer and metabolic activator technology enhances the passive diffusion and active transport mechanism through intestinal microvilli to improve Tinospora cordifolia absorption as well prevents its metabolic reduction through inhibition of P-gp efflux pump, ATPase activity, CYP3A4, CYP3AG and UGT.",
+    segment: "Human OTC",
+    type: "Capsule",
+    category: "Immunomodulator , Other Specialty",
+    packaging: "30, 60 and 90 tablet/Capsules Container",
+    Productimg: "/Assets/Product img/Auricur.jpeg",
+    Productname: "/Assets/Product name/Auricur.jpeg",
+    composition: [
+      "1 capsule",
+      "Curcumin Nanophosphosome®",
+      "and Tinospora cordifolia",
+      "* (Auricur®Plus) (Curcumin",
+      "Phosphatidylcholine -",
+      " Ethanolamine - Inositol - Serine",
+      "complex) 210 mg",
+      "Tinospora cordifolia extract 100 mg",
+      "Curcuminoids 95 mg",
+      "Alkaloids and Diterpenoid Bitters 10 mg",
+      "Daily Value not established",
+      "* Fortified with Turmerone, Piperine and",
+      "Cuminum cyminum",
+      "Other ingredients: Calcium carbonate,",
+      "Permitted class II preservative."
+    ],
+    indications:
+      "Reports suggest that Curcumin and Tinospora cordifolia supplementation improves immunity, facilitates recovery during pain, inflammation, Rheumatoid arthritis, helps in wound healing & Diabetes management. Auricur® Plus is a combination of Curcumin Nanophosphosome and Tinospora cordifolia with technologically advanced bioenhancer and metabolic potentiator technology.",
+    usage:
+      "1 tablet/capsule twice daily with warm milk/water (morning/evening). Or as directed by qualified healthcare professional. This product is not intended to cure or prevent any disease.",
+    report: "Lab tested for purity and effectiveness.",
+    brochure: "Download our product brochure for details.",
+    feedback: "Customer reviews and testimonials."
   }
 ];
