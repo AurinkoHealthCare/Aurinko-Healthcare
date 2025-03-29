73b1d85626fc989a9ee15519aec093d62652bab0
AurinkoHealthCare <aurinkohealthcare2014@gmail.com>
2025-03-29 18:52:29 +0530

    some changes in frontend data 29-03-2025/3

--- frontend/src/Pages/About us/ResearchDevelopment.jsx
+++ frontend/src/Pages/About us/ResearchDevelopment.jsx
@@ -8,7 +8,7 @@ const ResearchDevelopment = () => {
           loading="lazy"
           src="/Assets/banner/Research and developement.jpg"
           alt="Scientists working in a lab"
-          className="w-full h-[400px] object-cover object-top"
+          className="w-full"
         />
         <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center">
           <h1 className="text-white text-3xl md:text-4xl lg:text-5xl font-bold">
